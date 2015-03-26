eca19f4ff6c1aff7eb562f3b867ae98db4aa90b3
Anastasios Chouliaropoulos <T-Work1@Tasos.local>
2015-03-26 13:06:22 +0100

    Configure the DB connection
    Add the beans for the datasource and the getmessages implementation

--- src/main/java/com/tradeshift/resources/AppConfig.java
+++ src/main/java/com/tradeshift/resources/AppConfig.java
@@ -1,9 +1,14 @@
 package com.tradeshift.resources;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import com.tradeshift.service.HelloWorldService;
+import com.tradeshift.service.MessagesDAO;
+import com.tradeshift.service.MessagesDAOImpl;
 import com.tradeshift.service.SpringHelloWorldService;
 
 @Configuration
@@ -13,5 +18,21 @@ public class AppConfig{
     public HelloWorldService getHelloWorldService() {
 
         return new SpringHelloWorldService();
+    }
+
+    @Bean
+    public DataSource getDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/messagesdb");
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("password");
+
+        return dataSource;
+    }
+
+    @Bean
+    public MessagesDAO getMessagesDAO() {
+        return new MessagesDAOImpl(getDataSource());
     }
 }
--- src/main/java/com/tradeshift/service/SpringHelloWorldService.java
+++ src/main/java/com/tradeshift/service/SpringHelloWorldService.java
@@ -1,8 +1,12 @@
 package com.tradeshift.service;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class SpringHelloWorldService implements HelloWorldService {
 
+    @Autowired
+    private MessagesDAO messagesDAO;
 
     @Override
     public String getHelloWorldMessage(String name){
