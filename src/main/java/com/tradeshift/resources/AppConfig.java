bd7be984589e38df26109892e33584a7cea19214
Anastasios Chouliaropoulos <T-Work1@Tasos.local>
2015-03-26 17:18:09 +0100

    Configure the connection with Postgresql DB

--- src/main/java/com/tradeshift/resources/AppConfig.java
+++ src/main/java/com/tradeshift/resources/AppConfig.java
@@ -5,6 +5,7 @@ import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
 import com.tradeshift.service.HelloWorldService;
 import com.tradeshift.service.MessagesDAO;
@@ -21,12 +22,17 @@ public class AppConfig{
     }
 
     @Bean
-    public DataSource getDataSource() {
+/*    public DataSource getDataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setDriverClassName("org.postgresql.jdbc.Driver");
         dataSource.setUrl("jdbc:postgresql://localhost:5432/messagesdb");
         dataSource.setUsername("postgres");
         dataSource.setPassword("password");
+
+        return dataSource;
+    }*/
+    DataSource getDataSource() {
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource(new org.postgresql.Driver(), "jdbc:postgresql://localhost:5432/messagesdb", "postgres", "password");
 
         return dataSource;
     }
