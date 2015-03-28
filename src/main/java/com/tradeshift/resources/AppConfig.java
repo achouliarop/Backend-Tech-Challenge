9da1f91669e0f5dfda820ff1181525fee9ee9bdc
Anastasios Chouliaropoulos <T-Work1@Tasos.lan>
2015-03-29 00:42:27 +0100

    Refactor and clean the configuration file

--- src/main/java/com/tradeshift/resources/AppConfig.java
+++ src/main/java/com/tradeshift/resources/AppConfig.java
@@ -4,13 +4,12 @@ import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
 import com.tradeshift.service.HelloWorldService;
+import com.tradeshift.service.HelloWorldServiceImpl;
 import com.tradeshift.service.MessagesDAO;
 import com.tradeshift.service.MessagesDAOImpl;
-import com.tradeshift.service.SpringHelloWorldService;
 
 @Configuration
 public class AppConfig{
@@ -18,26 +17,17 @@ public class AppConfig{
     @Bean(name = "helloWorldService")
     public HelloWorldService getHelloWorldService() {
 
-        return new SpringHelloWorldService();
+        return new HelloWorldServiceImpl();
     }
 
     @Bean
-/*    public DataSource getDataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.postgresql.jdbc.Driver");
-        dataSource.setUrl("jdbc:postgresql://localhost:5432/messagesdb");
-        dataSource.setUsername("postgres");
-        dataSource.setPassword("password");
-
-        return dataSource;
-    }*/
     DataSource getDataSource() {
         SimpleDriverDataSource dataSource = new SimpleDriverDataSource(new org.postgresql.Driver(), "jdbc:postgresql://localhost:5432/messagesdb", "postgres", "password");
 
         return dataSource;
     }
 
-    @Bean
+    @Bean(name = "messagesDAO")
     public MessagesDAO getMessagesDAO() {
         return new MessagesDAOImpl(getDataSource());
     }
