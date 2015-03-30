90a7c6dc092f1527c399dc3fa798c7cc8a181ec4
Anastasios Chouliaropoulos <T-Work1@Tasos.lan>
2015-03-30 20:39:34 +0200

    Add null checks

--- src/main/java/com/tradeshift/service/MessagesDAOImpl.java
+++ src/main/java/com/tradeshift/service/MessagesDAOImpl.java
@@ -7,6 +7,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 
 import javax.sql.DataSource;
+import javax.ws.rs.BadRequestException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
@@ -16,20 +17,18 @@ public class MessagesDAOImpl implements MessagesDAO {
     private JdbcTemplate jdbcTemplate;
 
     @Autowired
-    public MessagesDAOImpl(DataSource dataSource) {
-
-        this.jdbcTemplate = new JdbcTemplate(dataSource);
-    }
-
-    @Autowired
     public MessagesDAOImpl(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
 
     @Override
     public void insert(String name) {
-        String sql = "INSERT INTO message (name) VALUES (?)";
-        jdbcTemplate.update(sql, name);
+        if (name == null) {
+            throw new BadRequestException("Name can not be empty");
+        } else {
+            String sql = "INSERT INTO message (name) VALUES (?)";
+            jdbcTemplate.update(sql, name);
+        }
     }
 
     @Override
@@ -40,7 +39,7 @@ public class MessagesDAOImpl implements MessagesDAO {
                         "(SELECT * FROM message ORDER BY date DESC LIMIT 10) " +
                     "SELECT * FROM t ORDER BY date ASC";
 
-        List<DBMessage> dbMessages = jdbcTemplate.query(sql, new RowMapper<DBMessage>() {
+        List<DBMessage> dbMessages = jdbcTemplate.query(sql, new RowMapper<DBMessage>()  {
 
             @Override
             public DBMessage mapRow(ResultSet rs, int rowNum) throws SQLException {
