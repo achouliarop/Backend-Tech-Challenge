c4c2448f57852c036140d774cdcb3eb4a573e1dd
Anastasios Chouliaropoulos <T-Work1@Tasos.lan>
2015-03-30 20:36:59 +0200

    Refactor configuration

--- src/main/java/com/tradeshift/resources/AppConfig.java
+++ src/main/java/com/tradeshift/resources/AppConfig.java
@@ -20,25 +20,38 @@ public class AppConfig{
         return new HelloWorldServiceImpl();
     }
 
+
     @Bean
-    DataSource getDataSource() {
+    public JdbcTemplate jdbcTemplate(SimpleDriverDataSource dataSource) {
+        String sql = "CREATE TABLE IF NOT EXISTS message (message_id serial NOT NULL, name text NOT NULL, date timestamp without time zone DEFAULT now(), CONSTRAINT message_pkey PRIMARY KEY (message_id))";
+
+        if (dataSource == null) {
+            throw new IllegalArgumentException();
+        } else {
+            JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+            jdbcTemplate.update(sql);
+
+            return jdbcTemplate;
+        }
+    }
+
+    @Bean
+    public SimpleDriverDataSource dataSource() {
         String addr = System.getenv("DB_PORT_5432_TCP_ADDR");
         String port = System.getenv("DB_PORT_5432_TCP_PORT");
         if (addr == null || port == null) {
             throw new IllegalArgumentException("Can not connect to postgres using variables  "  + "\n env:" + System.getenv());
         }
 
-        SimpleDriverDataSource dataSource = new SimpleDriverDataSource(new org.postgresql.Driver(), "jdbc:postgresql://"+ addr+":"+port+ "/messagesdb", "messagesdb", "password");
-        String sql = "CREATE TABLE IF NOT EXISTS message (message_id serial NOT NULL, name text NOT NULL, date timestamp without time zone DEFAULT now(), CONSTRAINT message_pkey PRIMARY KEY (message_id))";
-
-        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
-        jdbcTemplate.update(sql);
-
-        return dataSource;
+        return new SimpleDriverDataSource(new org.postgresql.Driver(), "jdbc:postgresql://"+ addr+":"+port+ "/messagesdb", "messagesdb", "password");
     }
 
     @Bean(name = "messagesDAO")
-    public MessagesDAO getMessagesDAO() {
-        return new MessagesDAOImpl(getDataSource());
+    public MessagesDAO getMessagesDAO(JdbcTemplate jdbcTemplate) {
+        if (jdbcTemplate == null) {
+            throw new IllegalArgumentException();
+        } else {
+            return new MessagesDAOImpl(jdbcTemplate);
+        }
     }
 }
