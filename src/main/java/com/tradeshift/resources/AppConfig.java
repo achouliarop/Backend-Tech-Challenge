c7ed67560c5d3c6e139c33dd5c2ce93e3062278e
Anastasios Chouliaropoulos <T-Work1@Tasos.local>
2015-03-30 13:58:57 +0200

    Update the @Configuration with the option to initiate the Message table on startup

--- src/main/java/com/tradeshift/resources/AppConfig.java
+++ src/main/java/com/tradeshift/resources/AppConfig.java
@@ -1,9 +1,11 @@
 package com.tradeshift.resources;
 
+import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
 import com.tradeshift.service.HelloWorldService;
@@ -22,7 +24,25 @@ public class AppConfig{
 
     @Bean
     DataSource getDataSource() {
-        SimpleDriverDataSource dataSource = new SimpleDriverDataSource(new org.postgresql.Driver(), "jdbc:postgresql://localhost:5432/messagesdb", "postgres", "password");
+        String addr = System.getenv("DB_PORT_5432_TCP_ADDR");
+        String port = System.getenv("DB_PORT_5432_TCP_PORT");
+        if (addr == null || port == null) {
+            throw new IllegalArgumentException("Can not connect to postgress using variables  "  + "\n env:" + System.getenv());
+        }
+
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource(new org.postgresql.Driver(), "jdbc:postgresql://"+ addr+":"+port+ "/messagesdb", "messagesdb", "password");
+        String sql = "DROP TABLE IF EXISTS message;\n" +
+                "\n" +
+                "CREATE TABLE message\n" +
+                "(\n" +
+                "  message_id serial NOT NULL,\n" +
+                "  name text NOT NULL,\n" +
+                "  date timestamp without time zone DEFAULT now(),\n" +
+                "  CONSTRAINT message_pkey PRIMARY KEY (message_id)\n" +
+                ")";
+
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        jdbcTemplate.update(sql);
 
         return dataSource;
     }
